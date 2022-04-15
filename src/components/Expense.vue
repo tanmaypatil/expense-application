b4f27acb3660eaaac8e9cfd40661d4f4b00a3258
Tanmay Patil <tany.patil77@gmail.com>
2022-04-15 23:45:41 +0530

    expense form - added controls

--- src/components/Expense.vue
+++ src/components/Expense.vue
@@ -1,50 +1,44 @@
 <template>
-  <div class="hello">
-    <h1>Submit a expense</h1>
-  
-    <el-row class="mb-4">
-      <el-col :span="4">
-        <label>Enter expense date</label>
+ <h1> Submit a expense </h1>
+  <el-form :model="form" label-width="120px" style="max-width: 460px">
+    <el-form-item label="Expense date">
+      <el-col :span="11">
+        <el-date-picker v-model="form.expense_date" type="date" placeholder="Pick a date" 
+         format="YYYY/MM/DD"
+        value-format="YYYY-MM-DD" style="width: 100%" />
       </el-col>
-      <el-col :span="8">
-        <el-date-picker v-model="value1" type="date" placeholder="Pick a day" />
-      </el-col>
-    </el-row>
+    </el-form-item>
+    <el-form-item label="Exepnse type">
+      <el-select v-model="form.expense_type" placeholder="please select expense type">
+        <el-option label="food" value="food" />
+        <el-option label="movie" value="movie" />
+        <el-option label="travelling" value="movie" />
+        <el-option label="books" value="movie" />
+      </el-select>
+    </el-form-item>
+    <el-form-item label="Expense amount">
+    <el-input-number v-model="form.num" :min="1" :max="1000"  />
+    </el-form-item>
+    <el-form-item label="Expense details">
+      <el-input v-model="form.desc" type="textarea" />
+    </el-form-item>
+    <el-form-item>
+      <el-button type="primary" @click="onSubmit">submit expense</el-button>
+      <el-button>Cancel</el-button>
+    </el-form-item>
+  </el-form>
+  <el-table :data="tableData" stripe style="width: 100%">
+    <el-table-column prop="date" label="expense Date" width="180" />
+    <el-table-column prop="desc" label="Expense Desc" width="180" />
+    <el-table-column prop="amount" label="amount" />
+  </el-table>
 
-    <el-row class="mb-4">
-      <el-col :span="4">
-        <label>Enter expense type</label>
-      </el-col>
-      <el-col :span="8">
-        <el-select v-model="expenseOpt" class="m-2" placeholder="Select" size="small">
-          <el-option
-            v-for="item in options"
-            :key="item.value"
-            :label="item.label"
-            :value="item.value"
-          />
-        </el-select>
-      </el-col>
-    </el-row>
-  <el-row class="mb-4">
-      <el-col :span="4">
-        <label>Enter expense amount</label>
-      </el-col>
-      <el-col :span="8">
-        <el-input v-model="amount" size="small" placeholder="Enter amount" />
-      </el-col>
-    </el-row>
-    <el-row class="mb-4">
-      <el-col :span="4">
-        <el-button v-on:click="convert" id="submit" type="primary">Submit expense</el-button>
-      </el-col>
-    </el-row>
-
-  </div>
 </template>
 
 <script>
+
 import axios from 'axios';
+import { ElMessage } from 'element-plus'
 export default {
   name: 'ExpenseApp',
   props: {
@@ -52,37 +46,31 @@ export default {
   },
   data() {
     return {
-      input: 0,
-      expenseOpt: 'food',
-      name: 'Expense',
-      value1: "2022/04/11",
-      output: 0,
-      amount : 0,
-      options: [
-        {
-          value: 'food',
-          label: 'food',
-        },
-        {
-          value: 'travelling',
-          label: 'travelling',
-        },
-        {
-          value: 'movie',
-          label: 'movie',
-        },
-        {
-          value: 'clothes',
-          label: 'clothes',
-        },
-        {
-          value: 'books',
-          label: 'books',
-        },
-      ]
-    }
+      form: {},
+      tableData : [
+  {
+    date: '2016-05-03',
+    desc: 'Tom',
+    amount: 100,
   },
+  {
+    date: '2016-05-02',
+    desc: 'Tom',
+    amount : 200,
+  },
+  {
+    date: '2016-05-04',
+    desc: 'Tom',
+    amount : 300,
+  },
+
+      ],
+  }},
   methods: {
+    onSubmit() {
+      console.log("doSubmit" + JSON.stringify(this.form));
+      ElMessage('expense submitted');
+    },
     convert() {
       console.log(`Hello ${this.name}!`);
       console.log(this.input);
@@ -111,15 +99,36 @@ export default {
 h3 {
   margin: 40px 0 0;
 }
+
 ul {
   list-style-type: none;
   padding: 0;
 }
+
 li {
   display: inline-block;
   margin: 0 10px;
 }
+
 a {
   color: #42b983;
+}
+
+.card-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+
+.text {
+  font-size: 14px;
+}
+
+.item {
+  margin-bottom: 18px;
+}
+
+.box-card {
+  width: 600px;
 }
 </style>
